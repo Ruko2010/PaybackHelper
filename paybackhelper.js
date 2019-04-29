be0cd8bbbe58adf4b2c4a3f3b7e7f62a88d9359e
Ruko2010 <gfemajor2@gmx.de>
2019-04-29 10:06:07 +0200

    Update Settings

--- paybackhelper.js
+++ paybackhelper.js
@@ -4,6 +4,8 @@
 // @version      0.1
 // @description  Activating all Coupons on Payback.com with one click
 // @author       Ruko2010
+// @copyright    2019 Ruko2010
+// @license      GNU General Public License v2.0
 // @include      http*://www.payback.de/pb/couponcenter/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // @require      http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js

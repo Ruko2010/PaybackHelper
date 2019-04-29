bcc5a2d175b3e2100b50506081b55a353c38833d
Ruko2010 <gfemajor2@gmx.de>
2019-04-29 10:02:02 +0200

    Create Script

---
+++ paybackhelper.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Activate All Payback Coupons
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Activating all Coupons on Payback.com with one click
+// @author       Ruko2010
+// @include      http*://www.payback.de/pb/couponcenter/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @require      http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var span = document.createElement("span");
+    span.innerHTML = '<input type="button" href="javascript:void(0);" title="Activate all Coupons" value="Activate all Coupons">';
+    span.addEventListener("click", function(){
+        alert('Starting the Work');
+        $('#notActivatedCoupons .not-activated').each(function(){
+            $(this).click();
+        });
+        alert('Done');
+    }, false);
+    $('#couponFilter').append(span);
+})();
